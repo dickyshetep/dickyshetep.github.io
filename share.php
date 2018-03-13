4b6d27da8400bbba3aa1001145ab481986338a69
dickysuryos <dickyshetep@gmail.com>
2018-03-13 11:33:41 +0700

    a
    a

--- share.php
+++ share.php
@@ -9,7 +9,7 @@ $highscore = $scorex[0];
 	<meta property="og:title" content="Game CHAO !"/>
 	<meta property="og:image" content="http://dickysuryo.me/logo.png"/>
 	<meta property="og:site_name" content="Game CHAO !"/>
-	<meta property="og:description" content="<?echo $highscore;?> score saya di GAME CHAO"/>
+	<meta property="og:description" content="<?php echo $highscore;?> score saya di GAME CHAO"/>
 </head>
 <body>
 	<meta http-equiv="refresh" content="0"; URL="http://dickysuryo.me/"/>
